cbc44173b34b4a25ea21d5ed0770129f1a9adf7b
azmikamis <azmi.stuff@gmail.com>
2015-02-05 18:36:31 +0800

    Update introvertscientists2.py

--- introvertscientists/introvertscientists2.py
+++ introvertscientists/introvertscientists2.py
@@ -3,20 +3,22 @@ def findnextseat(n,taken):
     longestgaplength = 0
     gapstart = 0
     gaplength = 0
-    for i,j in enumerate(sorted(taken)):
-        if len(taken) == 0:
-            longestgapstart = 1
-            longestgaplength = n
-        
+
+    if len(taken) == 0:
+        return 1
+    
+    l = sorted(taken)
+    if l[0] != 1:
+        l.insert(0, 0)
+    if l[len(l)-1] != n:
+        l.append(n+1)
+
+    for i,j in enumerate(l):
         if i == 0:
-            gapstart = 1 if j != 1 else j+1
-            if i == len(taken)-1:
-                gaplength = n-gapstart if j != n else n-gapstart-1
+            gapstart = j+1
+            continue
         else:    
-            if i == len(taken)-1:
-                gaplength = n-gapstart-1 if j != n else n-gapstart
-            else:
-                gaplength = j-gapstart
+            gaplength = j-gapstart
 
         if gaplength > longestgaplength:
             longestgaplength = gaplength
@@ -25,7 +27,7 @@ def findnextseat(n,taken):
 
     if longestgapstart == 1:
         return 1
-    elif longestgapstart + longestgaplength == n and n not in taken:
+    elif longestgapstart + longestgaplength - 1 == n and n not in l:
         return n
     else:
         return longestgapstart + (longestgaplength-1)/2
